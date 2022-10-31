8908c1640e43b4830ccea8aa2310c28126113b0e
Noahkimtai <noahkimtai24@gmail.com>
2022-11-01 00:29:48 +0300

    Completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        anagram = []
+        for i in array do
+            if(i.chars.sort.join==@word.chars.sort.join)
+                anagram << i
+            end
+        end
+        anagram
+    end
+end
